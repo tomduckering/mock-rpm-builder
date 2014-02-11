926b03d099bf41f7e2d754afa5fc6fb67c2f0e4f
Tom Duckering <tduckeri@thoughtworks.com>
2014-02-11 00:09:58 +0000

    Tom - adding back vagrant commands to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#vagrant up
-#vagrant provision
+vagrant up
+vagrant provision
 vagrant ssh-config > .rpmbuilder-ssh-config
 scp -F .rpmbuilder-ssh-config build-rpm.sh default:/tmp
 ssh -F .rpmbuilder-ssh-config default sh /tmp/build-rpm.sh
